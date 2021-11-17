d566ebe101021682e1fb4aa22c713dceccd672f6
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-17 21:11:16 +0900

    add shared dir

---
+++ shared/quic.py
@@ -0,0 +1,40 @@
+from mininet.net import Mininet
+from mininet.node import Controller, Host, Node, OVSKernelSwitch
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+
+def build():
+    net = Mininet()
+
+    info("*** Adding controller ***\n")
+    c0 = net.addController(name="c0", controller=Controller, protocol="tcp", port=6633)
+    info("done\n")
+
+    info("*** Adding switch ***\n")
+    s1 = net.addSwitch("s1", cls=OVSKernelSwitch)
+    info("done\n")
+
+    info("*** Adding host ***\n")
+    h1 = net.addHost("h1", ip="10.0.0.1/24")
+    h2 = net.addHost("h2", ip="10.0.0.2/24")
+    info("done\n")
+
+    info("*** Adding link ***\n")
+    link = {"cls": TCLink, "bw": 100}
+    net.addLink(h1, s1, **link)
+    net.addLink(h2, s1, **link)
+    info("\ndone\n")
+
+    net.start()
+
+    info("*** ping h1-h2 ***\n")
+    net.ping([h1, h2])
+
+    net.stop()
+
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    build()
