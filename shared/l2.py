097ed364b4b8e302fd632ae4175fdb402f6ed482
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-23 11:55:36 +0900

    dump  part of PACKET_IN

--- shared/l2.py
+++ shared/l2.py
@@ -1,7 +1,12 @@
+import datetime
+import json
+import os
+
 from ryu.base import app_manager
-from ryu.ofproto import ofproto_v1_0
 from ryu.controller import ofp_event
 from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
+from ryu.lib.packet.packet import Packet
+from ryu.ofproto import ofproto_v1_0
 
 
 class L2Switch(app_manager.RyuApp):
@@ -9,11 +14,12 @@ class L2Switch(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
+        self.datetime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        self.key = 0
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
-        print(str(msg.data))
         dp = msg.datapath
         ofp = dp.ofproto
         ofp_parser = dp.ofproto_parser
@@ -23,6 +29,7 @@ class L2Switch(app_manager.RyuApp):
         data = None
         if msg.buffer_id == ofp.OFP_NO_BUFFER:
             data = msg.data
+            self.__dump_packet(data)
 
         out = ofp_parser.OFPPacketOut(
             datapath=dp,
@@ -32,3 +39,16 @@ class L2Switch(app_manager.RyuApp):
             data=data
         )
         dp.send_msg(out)
+
+    def __dump_packet(self, data):
+        packet = Packet(data)
+        pretty_json = json.dumps(packet.to_jsondict(), indent=4, sort_keys=True)
+
+        print(pretty_json)
+
+        dump_dir = f"dump/{self.datetime}"
+        os.makedirs(dump_dir, exist_ok=True)
+        with open(f"{dump_dir}/{self.key}.dmp", "w") as f:
+            f.write(pretty_json)
+
+        self.key += 1
