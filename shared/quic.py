f815a8980d9d4f32fb607781caaa8daa89523564
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-03 12:57:37 +0900

    feat quic app

--- shared/quic.py
+++ shared/quic.py
@@ -1,35 +1,34 @@
 from mininet.link import TCLink
 from mininet.log import setLogLevel, info
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelSwitch
+from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.cli import CLI
 
 
 def build():
     net = Mininet()
 
-    info("*** Adding controller ***\n")
-    c0 = net.addController(name="c0", protocol="tcp", ip="192.168.56.1", port=6633)
-    info("done\n")
+    info("adding controller...\n")
+    net.addController(name="c0", controller=RemoteController, ip="127.0.0.1", port=6633, protocol="tcp")
 
-    info("*** Adding switch ***\n")
+    info("adding switch...\n")
     s1 = net.addSwitch("s1", cls=OVSKernelSwitch)
-    info("done\n")
 
-    info("*** Adding host ***\n")
+    info("adding host...\n")
     h1 = net.addHost("h1", ip="10.0.0.1/24")
     h2 = net.addHost("h2", ip="10.0.0.2/24")
-    info("done\n")
 
-    info("*** Adding link ***\n")
+    info("adding link...\n")
     link = {"cls": TCLink, "bw": 100}
     net.addLink(h1, s1, **link)
     net.addLink(h2, s1, **link)
-    info("\ndone\n")
+    info("\n")
 
     net.start()
 
-    info("*** ping h1-h2 ***\n")
-    net.ping([h1, h2])
+    info("send QUIC packet\n")
+    h1.cmd("./bin/echo_server/server &")
+    print(h2.cmd("./bin/echo_server/client -addr 10.0.0.1:4430"))
 
     net.stop()
 
