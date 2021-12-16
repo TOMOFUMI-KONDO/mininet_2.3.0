5a1f4bdcbc1376e5205dc9a6057bb0739bd58e82
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-16 22:20:24 +0900

    fix: specify port

--- shared/topo/group_table_lb.py
+++ shared/topo/group_table_lb.py
@@ -11,12 +11,12 @@ class GroupTableLbTopo(Topo):
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
 
-        self.addLink(s1, h1)
-        self.addLink(s1, s2)
-        self.addLink(s1, s3)
-        self.addLink(s4, h2)
-        self.addLink(s4, s2)
-        self.addLink(s4, s3)
+        self.addLink(s1, h1, 3)
+        self.addLink(s1, s2, 1, 1)
+        self.addLink(s1, s3, 2, 1)
+        self.addLink(s4, h2, 3)
+        self.addLink(s4, s2, 1, 2)
+        self.addLink(s4, s3, 2, 2)
 
 
 topos = {'group_table_lb_topo': (lambda: GroupTableLbTopo())}
