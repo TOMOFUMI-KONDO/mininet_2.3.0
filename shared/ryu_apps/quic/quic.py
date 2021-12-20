1ea1aeb73a842c5d754777ce89633601c46a9071
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-20 18:05:48 +0900

    chore: move quic app files

--- shared/ryu_apps/quic/quic.py
+++ shared/ryu_apps/quic/quic.py
@@ -1,36 +1,16 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import json
-
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.lib.packet import ether_types, in_proto
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import packet
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.lib.packet import ether_types, in_proto, ethernet, ipv4, packet
 from ryu.ofproto import ofproto_v1_3
 
 
-class LearningL4Switch(app_manager.RyuApp):
+class Quic(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     IGNORED_ETHER_TYPES = [ether_types.ETH_TYPE_LLDP, ether_types.ETH_TYPE_IPV6]
 
     def __init__(self, *args, **kwargs):
-        super(LearningL4Switch, self).__init__(*args, **kwargs)
+        super(Quic, self).__init__(*args, **kwargs)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -86,21 +66,22 @@ class LearningL4Switch(app_manager.RyuApp):
         dp.send_msg(out)
 
     def __handle_ip(self, pkt: packet.Packet, datapath, in_port: int):
-        ip = pkt.get_protocol(ipv4.ipv4)
-        ip_src = ip.src
-        ip_dst = ip.dst
-        ipproto = ip.proto
+        pass
+        # ip = pkt.get_protocol(ipv4.ipv4)
+        # ip_src = ip.src
+        # ip_dst = ip.dst
+        # ipproto = ip.proto
 
         # install a flow to avoid packet_in next time
-        if ipproto == in_proto.IPPROTO_UDP:
-            self.logger.info(
-                "ip-packet in datapath:%s ip_src:%s ip_dst:%s in_port:%s protocol:%s",
-                datapath.id,
-                ip_src,
-                ip_dst,
-                in_port,
-                ipproto,
-            )
+        # if ipproto == in_proto.IPPROTO_UDP:
+        #     self.logger.info(
+        #         "ip-packet in datapath:%s ip_src:%s ip_dst:%s in_port:%s protocol:%s",
+        #         datapath.id,
+        #         ip_src,
+        #         ip_dst,
+        #         in_port,
+        #         ipproto,
+        #     )
 
     def __add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
