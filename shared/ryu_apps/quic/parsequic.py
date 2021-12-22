3bc8444479d92c1f5bbdb0ec82bd1f46038770a9
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-22 13:02:46 +0900

    feat: update proto schema

--- shared/ryu_apps/quic/parsequic.py
+++ shared/ryu_apps/quic/parsequic.py
@@ -6,9 +6,13 @@ import parsequic_pb2 as pq
 def run(data: bytes, host="localhost", port=8080):
     channel = grpc.insecure_channel(f"{host}:{port}")
     stub = pqgrpc.ParseQuicStub(channel)
-    response = stub.Parse(pq.ParseQuicRequest(data=data))
-    print(response)
-    # print("is_long_header: " + str(response.is_long_header))
+    r = stub.Parse(pq.ParseQuicRequest(data=data))
+    print(
+        f"isLongHeader:{r.isLongHeader} "
+        f"type:{r.type} version:{pq.PacketType.Name(r.type)} "
+        f"dstConnID:{r.dstConnID} "
+        f"srcConnID:{r.srcConnID}\n"
+    )
 
 
 if __name__ == "__main__":
