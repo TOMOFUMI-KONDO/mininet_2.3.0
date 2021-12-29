96f5562238c9f9dbb1957271831503170379f792
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-29 15:18:00 +0900

    "fix: quic topo"

--- shared/topo/quic.py
+++ shared/topo/quic.py
@@ -4,43 +4,37 @@ from mininet.link import TCLink
 from mininet.log import setLogLevel, info
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.topo import Topo
 
 
-def build():
-    net = Mininet()
+class Quic(Topo):
+    def build(self):
+        s1 = self.addSwitch("s1")
+        h1 = self.addHost("h1", ip="10.0.0.1/24")
+        h2 = self.addHost("h2", ip="10.0.0.2/24")
 
-    info("adding controller...\n")
-    net.addController(name="c0", controller=RemoteController, ip="127.0.0.1", port=6633, protocol="tcp")
-
-    info("adding switch...\n")
-    s1 = net.addSwitch("s1", cls=OVSKernelSwitch)
-
-    info("adding host...\n")
-    h1 = net.addHost("h1", ip="10.0.0.1/24")
-    h2 = net.addHost("h2", ip="10.0.0.2/24")
-
-    info("adding link...\n")
-    link = {"cls": TCLink, "bw": 100}
-    net.addLink(h1, s1, **link)
-    net.addLink(h2, s1, **link)
-    info("\n")
-
-    net.start()
-
-    net.pingAll()
-
-    info("*** Starting QUIC server. Please wait... ***\n")
-    h1.cmd("./bin/server -addr 0.0.0.0:4430 &")
-    time.sleep(3)
-    info("*** Started! ***\n")
-
-    info("*** Send QUIC packet ***\n")
-    out = h2.cmd("./bin/client -addr 10.0.0.1:4430")
-    print(out)
-
-    net.stop()
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
 
 
 if __name__ == "__main__":
     setLogLevel("info")
-    build()
+
+    net = Mininet(
+        topo=Quic(),
+        controller=RemoteController("c0", port=6633)
+    )
+
+    net.start()
+
+    hosts = net.hosts
+    h0 = hosts[0]
+    h1 = hosts[1]
+
+    h0.cmd("./bin/server &")
+    time.sleep(3)  # wait to start server
+
+    out = h1.cmd(f"./bin/client -addr {h0.IP()}:4430")
+    print(out)
+
+    net.stop()
